c658ec971b8110494167d87c55aa05b3cef8a0c9
sandwich01 <startman71@gmail.com>
2025-05-20 22:12:01 +0300

    Улучший настройку jest + первые тесты для classNames

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-    clearMocks: true,
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-};
-
-export default config;
