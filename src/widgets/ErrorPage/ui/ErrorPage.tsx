cdeb28892bd6a61c2ba8db56ccb4287dab7f4ba1
sandwich01 <startman71@gmail.com>
2025-05-21 19:15:18 +0300

    add ErrorBoundary

---
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -0,0 +1,36 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import Button from 'shared/ui/Button/Button';
+import cls from './ErrorPage.module.scss';
+
+interface ErrorPageProps {
+    className?: string;
+}
+
+/**
+ * Компонент страницы ошибки, отображающий сообщение о критической ошибке и кнопку для перезагрузки страницы.
+ *
+ * @component
+ * @param {Object} props - Пропсы компонента.
+ * @param {string} [props.className] - Дополнительный CSS-класс для стилизации.
+ *
+ * @example
+ * <ErrorPage className="custom-error-style" />
+ */
+export const ErrorPage = ({ className }: ErrorPageProps) => {
+    const { t } = useTranslation('translation');
+
+    const reloadPage = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.ErrorPage, {}, [className])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button onClick={reloadPage}>
+                {t('Обновить страницу')}
+            </Button>
+        </div>
+    );
+};
