a5a711ebb97b5d765d33eb5b1a0385f00ab91225
sandwich01 <startman71@gmail.com>
2025-05-23 23:38:46 +0300

    new BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 /**
@@ -34,5 +35,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
         // Подключение плагина для перерисовки контента при разработке без перезагрузки страницы в браузере
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ];
 }
