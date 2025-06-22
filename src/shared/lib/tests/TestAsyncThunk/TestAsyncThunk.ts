2e818ea7d197e466e6d3da3a0e0e3e6e5e9ad529
sandwich01 <startman71@gmail.com>
2025-06-22 22:13:39 +0300

    Тесты для слайсов и селекторов логин формы

---
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -0,0 +1,72 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { AsyncThunkAction } from '@reduxjs/toolkit';
+
+/**
+ * Тип для функции создания async thunk action.
+ *
+ * @template Return - Тип успешного результата (`Return`)
+ * @template Arg - Тип аргумента, передаваемого в thunk (`Arg`)
+ * @template RejectedValue - Тип значения при ошибке (`RejectedValue`)
+ *
+ * @typedef {Function} ActionCreatorType
+ * @param {Arg} arg - Аргумент для вызова async thunk (`Arg`)
+ * @returns {AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>} Возвращает thunk-экшен
+ */
+type ActionCreatorType<Return, Arg, RejectedValue>
+    = (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>;
+
+/**
+ * Утилитный класс для тестирования async thunks в Redux.
+ *
+ * Предоставляет мокнутые методы `dispatch` и `getState`, а также возможность вызова thunk с тестовыми данными.
+ *
+ * @template Return - Тип успешного результата thunk (`Return`)
+ * @template Arg - Тип аргумента, передаваемого в thunk (`Arg`)
+ * @template RejectedValue - Тип значения при отклонении (`RejectedValue`)
+ */
+export class TestAsyncThunk<Return, Arg, RejectedValue> {
+    /**
+     * Мокнутый dispatch для проверки вызовов экшенов
+     *
+     * @type {jest.MockedFn<any>}
+     */
+    dispatch: jest.MockedFn<any>;
+
+    /**
+     * Мокнутый getState для предоставления тестового состояния
+     *
+     * @returns {StateSchema} Тестовое состояние хранилища
+     */
+    getState: () => StateSchema;
+
+    /**
+     * Созданный thunk-экшен, который будет тестироваться
+     *
+     * @type {ActionCreatorType<Return, Arg, RejectedValue>}
+     */
+    actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
+
+    /**
+     * Конструктор класса
+     *
+     * @param {ActionCreatorType<Return, Arg, RejectedValue>} actionCreator - Thunk-экшен для тестирования
+     */
+    constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
+        this.actionCreator = actionCreator;
+        this.dispatch = jest.fn();
+        this.getState = jest.fn();
+    }
+
+    /**
+     * Вызывает thunk-функцию с подготовленным окружением
+     *
+     * @param {Arg} arg - Аргумент, передаваемый в thunk (`Arg`)
+     * @returns {Promise<AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>>} Результат выполнения thunk
+     */
+    async callThunk(arg: Arg) {
+        const action = this.actionCreator(arg);
+        const result = await action(this.dispatch, this.getState, undefined);
+
+        return result;
+    }
+}
