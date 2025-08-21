4be1396501abd82ba2c2d66ecd17765832027531
sandwich01 <startman71@gmail.com>
2025-08-21 20:23:02 +0300

    Новая тема

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -23,7 +23,17 @@ export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let newTheme: Theme;
+        switch (theme) {
+            case Theme.DARK:
+                newTheme = Theme.LIGHT
+            case Theme.LIGHT:
+                newTheme = Theme.ORANGE
+            case Theme.ORANGE:
+                newTheme = Theme.DARK
+            default:
+                newTheme = Theme.LIGHT
+        }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
